b4963c68468e051b3e07e62fa783f314b68b5bcf
Eduardo Pereira <edup2306@gmail.com>
2018-04-09 18:17:14 +1000

    Adicionado Configuracao da api

--- src/main/java/com/br/bhouse/api/BhouseApiApplication.java
+++ src/main/java/com/br/bhouse/api/BhouseApiApplication.java
@@ -2,8 +2,12 @@ package com.br.bhouse.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.br.bhouse.api.config.property.BhouseApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BhouseApiProperty.class)
 public class BhouseApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/bhouse/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/br/bhouse/api/token/RefreshTokenPostProcessor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -16,9 +17,15 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import com.br.bhouse.api.config.property.BhouseApiProperty;
+
 @ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken>{
 
+	
+	@Autowired
+	private BhouseApiProperty apiProperty; 
+	
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
 		return returnType.getMethod().getName().equals("postAccessToken");
@@ -51,7 +58,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 
 		Cookie cookie = new Cookie("refreshToken", refreshToken);
 		cookie.setHttpOnly(true);
-		cookie.setSecure(false); //TODO mudar para true em producao;
+		cookie.setSecure(apiProperty.getSecurity().isEnableHttps());
 		cookie.setPath(req.getContextPath()+ "/oauth/token");
 		cookie.setMaxAge(200000);
 		
