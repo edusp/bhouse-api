4859b656705beeb7984d3e595d78a5b205cc612f
Eduardo Pereira <edup2306@gmail.com>
2018-08-14 12:58:46 +1000

    Added search by email and listAll

--- src/main/java/com/br/bhouse/api/resouces/UserResource.java
+++ src/main/java/com/br/bhouse/api/resouces/UserResource.java
@@ -1,8 +1,13 @@
 package com.br.bhouse.api.resouces;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +34,30 @@ public class UserResource {
 		
 		return ResponseEntity.ok(userRegistered);
 	}
+	
+	@GetMapping(params = "email")
+	public ResponseEntity<Usuario> getByUsername(String email) {
+		Optional<Usuario> user = usuarioRepository.findByEmail(email);
+		return user.isPresent() ? ResponseEntity.ok(user.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Usuario> getById(@PathVariable long id) {
+		Usuario user = usuarioRepository.getOne(id);
+		Optional<Usuario> userOptional = Optional.of(user);
+		return userOptional.isPresent() ? ResponseEntity.ok(userOptional.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> listAll() {
+		List<Usuario> users = usuarioRepository.findAll();
+		return users.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(users);
+	}
+	
+	
+	
+	
+	
 	
 
 }
