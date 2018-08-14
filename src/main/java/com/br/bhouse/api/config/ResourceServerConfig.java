1975339e5a0fa54f038afde797c75743967993be
Eduardo Pereira <edup2306@gmail.com>
2018-08-14 14:36:52 +1000

    Removed authentication to users resource

--- src/main/java/com/br/bhouse/api/config/ResourceServerConfig.java
+++ src/main/java/com/br/bhouse/api/config/ResourceServerConfig.java
@@ -39,7 +39,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/users").permitAll()
+		.antMatchers("/users/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
